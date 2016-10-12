fd43c5b4d73de58fa5a4902cb2abeaebdccebdfe
ElsaLee <elsa@elsademacbook-pro.local>
2016-10-12 12:45:14 +0800

    Change to another project .....

---
+++ Lambsister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lambsister
+//
+//  Created by elsa on 2016/10/12.
+//  Copyright © 2016年 elsaJSpeedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
